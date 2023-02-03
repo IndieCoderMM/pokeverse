25d20ea729febde70054a22a06b121d66bfaf49d
hthantoo <hthant00chk@gmail.com>
2023-02-03 15:11:00 +0630

    Create basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
+import PokeCard from './components/PokeCard';
 
 function App() {
+  const pokemons = [
+    {
+      name: 'clefairy',
+      id: 1,
+      sprites: {
+        front_default:
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png',
+      },
+      height: 23,
+      weight: 64,
+      base_experience: 5,
+    },
+    {
+      name: 'clefairy',
+      id: 2,
+      sprites: {
+        front_default:
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png',
+      },
+      height: 23,
+      weight: 64,
+      base_experience: 5,
+    },
+    {
+      name: 'clefairy',
+      id: 3,
+      sprites: {
+        front_default:
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png',
+      },
+    },
+    {
+      name: 'clefairy',
+      id: 4,
+      sprites: {
+        front_default:
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png',
+      },
+    },
+    {
+      name: 'clefairy',
+      id: 5,
+      sprites: {
+        front_default:
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png',
+      },
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="p-2">
+      <Row md={4}>
+        {pokemons.map((poke) => (
+          <Col className="p-1">
+            <PokeCard key={poke.id} pokemon={poke} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
