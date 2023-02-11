f4b3106810c83ac4eb189e4edec88cd9a0a60dde
heinthant <hthant00chk@gmail.com>
2023-02-11 19:54:42 +0630

    Change bgColor

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Provider } from 'react-redux';
-import store from './redux/configureStore';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
